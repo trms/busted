c5b02fb76015ef472136186a0f074e26713da3aa
Drew <drew.ditthardt@gmail.com>
2014-05-08 18:24:27 -0700

    cleaned up core a bit

--- src/core.lua
+++ src/core.lua
@@ -7,14 +7,9 @@ return function()
 
   local context = {env={busted = busted}}
   busted.context = context
-
   local ctx = context
 
-  local registered = {}
-  busted.registered = registered
-
-  local executors = {}
-  busted.executors = executors
+  busted.executors = {}
 
   function busted.publish(channel, ...)
     mediator:publish(channel, ...)
@@ -24,9 +19,11 @@ return function()
     mediator:subscribe(channel, callback, options)
   end
 
-  --ENV
   local function getEnv(self, key)
-    return self.env and self.env[key] or self.parent and getEnv(self.parent, key) or _G[key]
+    return
+      self.env and self.env[key] or
+      self.parent and getEnv(self.parent, key) or
+      busted.executors[key] or _G[key]
   end
 
   local function setEnv(self, key, value)
@@ -44,28 +41,25 @@ return function()
 
   local env = setmetatable({}, {__index=__index, __newindex=__newindex})
 
-  function busted.safe(typ, run, element, setenv)
+  function busted.safe(descriptor, run, element, setenv)
     if setenv and type(run) == "function" then setfenv(element.run, env) end
     ctx = element
     local ret = {xpcall(run, function(message)
       local trace = debug.traceback('', 2)
-      busted.publish({'error', typ}, element.name, run, element.parent, message, trace)
+      busted.publish({'error', descriptor}, element.name, run, element.parent, message, trace)
     end)}
     ctx = element.parent
     return unpack(ret)
   end
 
-
   function busted.register(descriptor, executor)
-    registered[#registered+1] = descriptor
-    executors[descriptor] = function(name, fn)
+    busted.executors[descriptor] = function(name, fn)
       if not fn then
         fn = name
         name = nil
       end
       busted.publish({'register', descriptor}, name, fn)
     end
-    env[descriptor] = executors[descriptor]
     busted.subscribe({'register', descriptor}, function(name, fn)
       if not ctx[descriptor] then ctx[descriptor] = {} end
       if not ctx.children then ctx.children = {} end
@@ -80,7 +74,7 @@ return function()
     if current.children then
       for _, v in pairs(current.children) do
         if v.executor then
-          busted.safe(v.descriptor, function() return v.executor(v) end, current)
+          busted.safe(v.descriptor, function() return v.executor(v) end, v)
         end
       end
     end
@@ -92,11 +86,8 @@ return function()
   end
 
   function busted.clearEnv()
-    registered = {}
-    busted.registered = registered
     executors = {}
     busted.executors = executors
-    env = setmetatable({busted = busted}, {__index=_G})
   end
 
   return busted
